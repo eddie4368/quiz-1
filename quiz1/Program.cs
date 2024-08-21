be02f0b516206cb133a3ef2ef62208cb82f31f99
eddie ortega <23edortega@gmail.com>
2024-08-21 12:47:43 -0500

    this quiz 1

---
+++ quiz1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your first name?");
+                
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("what is your last name?");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("hello " + firstname + "" + lastname);
+
+        }
+    }
+}
